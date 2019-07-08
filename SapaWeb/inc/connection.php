8806e8f2d61f35eb330d6643f0493dbc23262ad0
a <anhtu26091998@gmail.com>
2019-07-08 11:03:16 +0700

    â

--- SapaWeb/inc/connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$database = "sapa";
-$username = "root";
-$password = "";
-// Create inc
-$conn = mysqli_connect($servername, $username, $password, $database);
-mysqli_set_charset($conn, "utf8");
-?>
--- SapaWeb/js/scroll.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $("a[href*='#']:not([href='#])").click(function () {
-        let target = $(this).attr("href");
-        $('html,body').stop().animate({
-            scrollTop: $(target).offset().top
-        }, 500);
-        event.preventDefault();
-    });
-});
